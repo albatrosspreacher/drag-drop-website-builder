5d641815df6fb95b08ddf37e5a87b072194e4d35
Glitch (enthusiastic-quiet-jonquil) <none>
2021-04-14 07:17:43 +0000

    🎌🍇 added templates page

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/templates", (request, response) => {
+  response.sendFile(__dirname + "/views/templates.html");
+});
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);

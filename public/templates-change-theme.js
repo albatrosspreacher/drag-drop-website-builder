676c9efb4a225354425f1786ecd5d9f58186d32c
Restyled.io <commits@restyled.io>
2021-05-16 13:31:39 +0000

    Restyled by prettier

--- public/templates-change-theme.js
+++ public/templates-change-theme.js
@@ -1,12 +1,12 @@
-onmessage = e => {
+onmessage = (e) => {
   if (e.data == "/templates-light-mode.css") {
     // check if fetched file name corresponds to light mode
     postMessage("/templates-dark-mode.css"); // if yes, prompt the main thread
-                                             // to change to dark
+    // to change to dark
   }
   if (e.data == "/templates-dark-mode.css") {
     // check if fetched file name corresponds to dark mode
     postMessage("/templates-light-mode.css"); // if yes, prompt the main thread
-                                              // to change to light
+    // to change to light
   }
 };

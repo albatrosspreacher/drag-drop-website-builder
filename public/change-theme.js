e75c9cc5e466be9b029b03ab8d9372be61f45bfb
Glitch (enthusiastic-quiet-jonquil) <none>
2021-05-17 13:27:41 +0000

    🎮 added assets, fixed carousel, finalised templates

--- public/change-theme.js
+++ public/change-theme.js
@@ -1,4 +1,4 @@
-onmessage = (e) => {
+onmessage = e => {
   if (e.data == "/index-light-mode.css") {
     // check if fetched file name corresponds to light mode
     postMessage("/index-dark-mode.css"); // if yes, prompt the main thread to
